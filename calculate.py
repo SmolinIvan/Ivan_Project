7d3e1a922f092f7d8215cb3dc7c73c3538e1082f
Ivan Smolin <smolin9667@gmail.com>
2023-03-03 14:35:15 +0300

    Calculate function

---
+++ calculate.py
@@ -0,0 +1,7 @@
+class Solution:          #объявляем класс
+    def calculate(a):    #объявляем функцию
+        return eval(a)   #вычисление ввода
+    
+
+print(Solution.calculate(input()))    
+    
--- feature_1.py
+++ feature_1.py
@@ -1,1 +1,5 @@
-print('Hello')
+from humanfriendly import format_size, parse_size
+from humanfriendly.prompts import prompt_for_input
+user_input = prompt_for_input("Enter a readable file size: ")
+num_bytes = parse_size(user_input)
+print(num_bytes)
